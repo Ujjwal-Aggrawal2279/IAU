35b6dd6948ee44bff07e3eed39f35f1e57caf2d4
frappe <frappe@ubuntu-22-erpnext-15.myguest.virtualbox.org>
2024-12-14 17:53:58 +0530

    removed the comment

--- iau/templates/includes/login.js
+++ iau/templates/includes/login.js
@@ -36,10 +36,10 @@ login_form_ele.addEventListener('submit', async (event) => {
         if (response.ok) {
             const result = await response.json();
             if (new URLSearchParams(window.location.search).get("redirect-to") && result.message.redirect_url !== "/Login") {
-                // window.location.href = new URLSearchParams(window.location.search).get("redirect-to");
+                window.location.href = new URLSearchParams(window.location.search).get("redirect-to");
                 return; 
             } else if(new URLSearchParams(window.location.search).get("redirect-to") && result.message.redirect_url === "/Login"){
-                // location.reload();
+                location.reload();
             } else {
                 window.location.href = result.message.redirect_url;
             }
