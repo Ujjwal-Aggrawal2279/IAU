fa10916249dab431ad45149b905e59a27f55a31f
frappe <frappe@ubuntu-22-erpnext-15.myguest.virtualbox.org>
2024-12-23 20:23:41 +0530

    changes for adding the cairo style for entire app

--- iau/hooks.py
+++ iau/hooks.py
@@ -26,7 +26,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/iau/css/iau.css"
-
+app_include_css = "/assets/iau/css/cairo_style.css"
 # include js, css files in header of web template
 # web_include_css = "/assets/iau/css/iau.css"
 # web_include_js = "/assets/iau/js/iau.js"
