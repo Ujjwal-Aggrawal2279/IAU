21b775952bd43fc274101273076e4a416e4af04c
frappe <frappe@ubuntu-22-erpnext-15.myguest.virtualbox.org>
2025-02-15 16:06:59 +0530

    added translation for login page

--- iau/templates/includes/login.js
+++ iau/templates/includes/login.js
@@ -8,7 +8,21 @@ pswd_toggler.addEventListener('click', () => {
     else {
         password_input.style.webkitTextSecurity = "none";
     }
-})
+});
+const login_page_dont_have_account_ele = document.getElementById('login_page_dont_have_account');
+if(preferred_language_value==="ar"){
+    login_page_dont_have_account_ele.innerHTML = `
+    ليس لديك حساب؟  &nbsp;<a id="login_page_signup_button" href="/login#signup"
+    style="text-decoration: underline; font-family : 'Encode Sans Condensed', system-ui; font-weight : 700; font-size : 1.25rem; color : #101423;">اشتراك</a>
+    `;
+}
+else{
+    login_page_dont_have_account_ele.innerHTML = `
+    Don’t have an account?&nbsp;<a id="login_page_signup_button" href="/login#signup"
+    style="text-decoration: underline; font-family : 'Encode Sans Condensed', system-ui; font-weight : 700; font-size : 1.25rem; color : #101423;">Sign
+    up</a>
+    `;
+}
 
 // login form
 const login_form_ele = document.querySelector('#login_form');
